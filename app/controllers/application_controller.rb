d8e1bcb374e7cd4eba8d8e7b079de2b5a64c6654
Fer Hernandez <fh.1111@gmail.com>
2019-01-03 02:17:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
 
-      @super_hero = members.collect do |member_params|
+      @super_heroes = members.collect do |member_params|
           SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
       end
       erb :team
